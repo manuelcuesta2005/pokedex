ce1ee5467bd3932c3e0cfd84ca4023ba016a9205
manuelcuesta2005 <macuba2005@gmail.com>
2023-09-20 14:40:06 -0500

    primer commit

---
+++ src/components/CardPokemon/index.jsx
@@ -0,0 +1,11 @@
+import './style.css'
+
+export default function CardPokemon(  ){
+
+    
+    return(
+        <section className='container'>
+            <img src="https://pokeapi.co/api/v2/pokemon/1/"/>
+        </section>
+    )
+}
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,30 @@
+import axios from "axios"
+import { useEffect } from "react"
+import { useState } from "react"
+
+export const cardPokemonsingle = [name, id, type]
+export const cardPokemonModel = [cardPokemonsingle]
+
+export default function Home(){
+    const [pokemon, setPokemon] = useState(cardPokemonModel) 
+    
+    useEffect(() => {
+        axios.get('https://pokeapi.co/api/v2/pokemon').then((data) =>{
+            setPokemon(data)
+            console.log(data)
+        }).catch((error) => console.error(error))
+    }, [])
+
+    const getPokemon = pokemon.map((Pokemon) =>{
+        <li key={Pokemon.id}>{Pokemon.name}</li>
+    })
+
+    return(
+            <div>
+            <h1>Lista de Publicaciones</h1>
+            <ul>
+                {getPokemon}
+            </ul>
+          </div>
+    )
+}
